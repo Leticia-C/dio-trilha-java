4aed51beb1165bebb4930d73288a47ad0e681bc6
Leticia-C <leticia.camposg93@gmail.com>
2023-03-05 12:32:03 -0300

    Adiciona exercicio Fatorial

---
+++ DioJavaCourse/dio-trilha-java/EstruturasDeRepetiçãoEArraysEmJava/loops/Fatorial.java
@@ -0,0 +1,18 @@
+package EstruturasDeRepetiçãoEArraysEmJava.loops;
+
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Fatorial");
+            int fatorial = scan.nextInt();
+            int multiplicacao = 1;
+            for(int i = fatorial; i > 0; i --) {
+                multiplicacao = multiplicacao * i ;
+            }
+            System.out.println(multiplicacao);
+        }
+    }
+  
+}
