a69f67fa3fffb14589beb96cbcb89c26f7f271d4
Leticia-C <leticia.camposg93@gmail.com>
2023-03-05 12:47:16 -0300

    Adiciona repositorio de exercicios de array e adiciona o exercicio OrdemInversa

---
+++ DioJavaCourse/dio-trilha-java/EstruturasDeRepetiçãoEArraysEmJava/arrays/OrdemInversa.java
@@ -0,0 +1,11 @@
+package EstruturasDeRepetiçãoEArraysEmJava.arrays;
+
+public class OrdemInversa {
+    public static void main(String[] args) {
+        int[] vetor = {1,2,3,4,5, 6};
+
+       for (int i = vetor.length - 1; i >= 0; i --) {
+       System.out.println(vetor[i]);
+       }
+    }
+}
