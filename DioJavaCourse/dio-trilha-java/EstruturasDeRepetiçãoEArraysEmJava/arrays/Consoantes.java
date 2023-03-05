080a7f2a0130a5203ef7e856aa330ef2ea93db3a
Leticia-C <leticia.camposg93@gmail.com>
2023-03-05 13:36:38 -0300

    Adiciona exercicio de Consoantes

---
+++ DioJavaCourse/dio-trilha-java/EstruturasDeRepetiçãoEArraysEmJava/arrays/Consoantes.java
@@ -0,0 +1,33 @@
+package EstruturasDeRepetiçãoEArraysEmJava.arrays;
+
+import java.util.Scanner;
+
+public class Consoantes {
+    public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+      String[] consoantes = new String[6];
+      int quantidaDeDeConsolantes = 0;
+      int count = 0;
+      do{
+        System.out.println("Digite uma letra");
+        String letra = scan.nextLine();
+        if(!(letra.equalsIgnoreCase("a") |
+        letra.equalsIgnoreCase("e") |
+        letra.equalsIgnoreCase("i") |
+        letra.equalsIgnoreCase("o") |
+        letra.equalsIgnoreCase("u") )
+        ) {
+            quantidaDeDeConsolantes ++;
+            consoantes[count] = letra;
+        }
+
+        count ++;
+      }while(count < consoantes.length);
+      System.out.println("A quantidade de consoantes é  " + quantidaDeDeConsolantes);
+
+      for (String consoante : consoantes) {
+        if( consoante != null) System.out.println(consoante + "  ");
+      }
+     
+      }
+}
