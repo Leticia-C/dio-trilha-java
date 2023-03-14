ca2e32668159b50c32c873616572c017704d84e7
Leticia-C <leticia.camposg93@gmail.com>
2023-03-14 17:08:47 -0300

    Entrega desafio da classe Triangulo

---
+++ DioJavaCourse/DominandoAlgaritimosIntermediariosComDesafiosJava/Triangulo.java
@@ -0,0 +1,39 @@
+package DominandoAlgaritimosIntermediariosComDesafiosJava;
+
+// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma: 
+// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next";
+// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.  
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Triangulo{
+	
+	public static void main(String[] args) throws IOException {
+		Scanner leitor = new Scanner(System.in);
+		double A = leitor.nextDouble();
+		double B = leitor.nextDouble();
+		double C = leitor.nextDouble();
+		double maior;
+		double soma;
+		boolean triangulo;
+		
+	  if(A > B && A > C)  {maior = A; soma = B + C;}
+	  if(A < B && B > C)  {maior = B;  soma = A + C;}
+		else maior = C; soma = A + B;
+		
+		if( soma > maior) {
+            triangulo = true;
+		  System.out.print("Perimetro = " + (soma + maior));
+		} else {
+            triangulo = false;
+		   System.out.print("Area = " + (( A + B) * C)/ 2);
+		  };
+//TODO: Implemente a condição necessária para o cálculo do triângulo:
+//Dica: Você pode utilizar o String.format() na formatação do texto.
+		
+		
+	}
+	
+}
